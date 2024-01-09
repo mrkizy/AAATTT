3fb0edeec3300090b4b2d9f61d0582093edce426
xuzhi <zhixu@lilith.com>
2024-01-09 18:06:24 +0800

    feat: modify some codes

---
+++ AAATTT.xcframework/ios-x86_64-simulator/AAATTT.framework/Headers/AAATTT.h
@@ -0,0 +1,18 @@
+//
+//  AAATTT.h
+//  AAATTT
+//
+//  Created by LilithGames on 2024/1/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AAATTT.
+FOUNDATION_EXPORT double AAATTTVersionNumber;
+
+//! Project version string for AAATTT.
+FOUNDATION_EXPORT const unsigned char AAATTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAATTT/PublicHeader.h>
+
+
